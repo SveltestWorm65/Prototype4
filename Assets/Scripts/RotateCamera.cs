677e68c20ee470405b7220ee8bace5f8be0f4888
SveltestWorm65 <wormgamedevelopment@gmail.com>
2023-10-24 10:03:41 -0700

    prototype 4.2 complete
    all steps done

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+   
     public float rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
